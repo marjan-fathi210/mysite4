b2e5b3f0b1611792c85603739d60fb25858c2c89
Marjan <marjanfathi210@gmail.com>
2026-06-26 18:49:35 +0430

    create a template folder in app
    html pages in template folder and in views.py in app give them by render

--- website/views.py
+++ website/views.py
@@ -3,7 +3,11 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse, JsonResponse
 
-def http_test(request):
-    return HttpResponse('<h1>This is a test</h1>')
-def json_test(request):
-    return JsonResponse({'name': 'marjan'})
+def index_view(request):
+    return render(request,'website/index.html')
+
+def about_view(request):
+    return render(request,'website/about.html')
+
+def contact_view(request):
+    return render(request,'website/contact.html')
