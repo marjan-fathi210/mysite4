d34241d77f1399e4e35b30f53fd3f655616c0eb1
Marjan <marjanfathi210@gmail.com>
2026-07-03 14:26:35 +0430

    in single blog pass data from view to blog-single.html
    in views.py in app blog added context to render's elements and in single-blog.html use {{key value}}

--- blog/views.py
+++ blog/views.py
@@ -5,4 +5,5 @@ def blog_view(request):
     return render(request,'blog/blog-home.html')
 
 def blog_single(request):
-    return render(request,'blog/blog-single.html')
+    context={'title':'bicoin crashed again!','content':'bitcoin was flying but now grounded,ok','author':'marjan fathi'}
+    return render(request,'blog/blog-single.html',context)
