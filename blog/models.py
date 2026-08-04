b131b4329d9f9ecb2efad1467d2b5b7c6e2e637f
Marjan <marjanfathi210@gmail.com>
2026-08-04 20:56:59 +0430

    panel admin customization
    in admin.py I can add options with PostAdmin class. and apply with admin.site.register

--- blog/models.py
+++ blog/models.py
@@ -13,8 +13,13 @@ class Post(models.Model):
     published_date= models.DateTimeField(null= True)
     created_date= models.DateTimeField(auto_now_add= True)
     updated_date= models.DateTimeField(auto_now= True)
+    class Meta:
+        ordering= ['-created_date']
+        #verbose_name= 'پست'
+        #verbose_name_plural= 'پست ها'
     
     # SELECT * FROM Post where status= 1
     def __str__(self):
         return "{}-{}".format(self.title, self.id)
+    
         
