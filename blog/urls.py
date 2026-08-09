5a32153d1b005a90b6e278292977b6d7c03033fe
Marjan <marjanfathi210@gmail.com>
2026-08-09 20:13:28 +0430

    url parameters
    I can pass post id to urls.py to show one post in single page

--- blog/urls.py
+++ blog/urls.py
@@ -6,6 +6,6 @@ app_name='blog'
 urlpatterns = [
     path('',blog_view, name='index'),
     path('single',blog_single, name='single'),
-    path('test',test_view, name='test')
+    path('post-<int:pid>',test_view, name='test')
 
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,17 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from blog.models import Post
+from datetime import datetime
 
 # Create your views here.
 def blog_view(request):
-    posts= Post.objects.filter(status=1)
-    context={'posts': posts}
-    return render(request,'blog/blog-home.html', context)
+    posts= Post.objects.all()
+    pub_date= posts.published_date
+    now_date= datetime.now()
+    if pub_date <= now_date:
+        context={'posts': posts}
+        return render(request,'blog/blog-home.html', context)
 
 def blog_single(request):
     context={'title':'bicoin crashed again!','content':'bitcoin was flying but now grounded,ok','author':'marjan fathi'}
     return render(request,'blog/blog-single.html',context)
 
-def test_view(request):
-    posts= Post.objects.all()
-    context={'posts': posts}
+def test_view(request, pid):
+    #post= Post.objects.get(id= pid)
+    post= get_object_or_404(Post, pk= pid)
+    context={'post': post}
     return render(request, 'test.html', context)
