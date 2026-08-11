3a47bc9af79c834c8a5c03e69776f1ef37a1607a
Marjan <marjanfathi210@gmail.com>
2026-08-11 22:15:49 +0430

    ManyToManyField
    I created category class and define category attribute in post table. with ManytoManyField join these tables.
    and show in blog template

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from blog.models import Post
+from blog.models import Post, Category
 
 # Register your models here.
 class PostAdmin(admin.ModelAdmin):
@@ -12,4 +12,5 @@ class PostAdmin(admin.ModelAdmin):
     search_fields= ['title', 'content']
 
 admin.site.register(Post, PostAdmin)
+admin.site.register(Category)
 
