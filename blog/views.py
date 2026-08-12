9bdcb520899dad9ec58274a333096481f62e4b0f
Marjan <marjanfathi210@gmail.com>
2026-08-12 12:43:05 +0430

    How in single post view increase counted_views in post table?
    this is an exercise for show post with published_date allowed. and increase counted_views with view a post

--- blog/views.py
+++ blog/views.py
@@ -4,13 +4,15 @@ from django.utils import timezone
 
 # Create your views here.
 def blog_view(request):
-    posts= Post.objects.filter(published_date__lte= timezone.now())
+    posts= Post.objects.filter(published_date__lte= timezone.now(), status=1)
     context={'posts': posts}
     return render(request,'blog/blog-home.html', context)
 
 def blog_single(request, pid):
     posts= Post.objects.filter(status=1)
-    post= get_object_or_404(posts, pk= pid, status=1)
+    post= get_object_or_404(posts, pk= pid)
+    post.counted_views+=1
+    post.save()
     context={'post': post}
     return render(request,'blog/blog-single.html',context)
 
