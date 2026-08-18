a8775d11c1120f906873be2351c5a17ab82f6f45
Marjan <marjanfathi210@gmail.com>
2026-08-18 20:43:47 +0430

    display posts based on category filter
    added category url and in view filter posts base on category name. and in blog-home template added static url in href of link related to each category

--- blog/views.py
+++ blog/views.py
@@ -23,7 +23,12 @@ def blog_single(request, pid):
     context={'post': post, 'prev_post': prev_post, 'next_post':next_post}
     return render(request,'blog/blog-single.html',context)
 
+def blog_category(request,cat_name):
+    posts= Post.objects.filter(status=1)
+    posts= posts.filter(category__name= cat_name)
+    context={'posts':posts}
+    return render(request, 'blog/blog-home.html', context)
+
 def test_view(request):
     return render(request,'test.html')
-    
-    
+        
