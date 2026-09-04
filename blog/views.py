7993b7749e807142ace1055eb7ffd4e2fd5c23cd
Marjan <marjanfathi210@gmail.com>
2026-09-04 12:37:55 +0430

    pagination in view and in template
    import Paginator in view and pagination posts. in template, object is posts. by if condition check the page has previous or has next. by a for loop we can show page numbers

--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,7 @@ from blog.models import Post
 from blog.models import Category
 from django.utils import timezone
 from django.http import HttpResponse, JsonResponse
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 # Create your views here.
 def blog_view(request, **kwargs):
@@ -11,6 +12,15 @@ def blog_view(request, **kwargs):
         posts= posts.filter(category__name= kwargs['cat_name'])
     if kwargs.get('author_username')!=None:
         posts= posts.filter(author__username= kwargs['author_username'])
+    #pagination
+    posts= Paginator(posts,3)
+    try:
+        page_number= request.GET.get('page')
+        posts=posts.page(page_number)
+    except PageNotAnInteger:
+        posts=posts.get_page(1)
+    except EmptyPage:
+        posts= posts.get_page(1)
 
     context={'posts': posts}
     return render(request,'blog/blog-home.html', context)
