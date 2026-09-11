63f4cf6817a7c44a4c0cf7682f0f3fceec88ae2f
Marjan <marjanfathi210@gmail.com>
2026-09-11 19:24:01 +0430

    create contact form and newsletter form
    in website app added a new file as forms.py. in forms.py created two class contact form and newsletter form that these class are modelform. before we added a new table in model as Newsletter and register this in admin.py . in views.py fill form by user request and return it. in template correct action form and use csrf_token

--- website/admin.py
+++ website/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from website.models import  Contact
+from website.models import  Contact,Newsletter
 
 class ContactAdmin(admin.ModelAdmin):
     date_hierarchy='created_date'
@@ -9,4 +9,5 @@ class ContactAdmin(admin.ModelAdmin):
 
 # Register your models here.
 admin.site.register(Contact,ContactAdmin)
+admin.site.register(Newsletter)
 
--- website/views.py
+++ website/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-
-# Create your views here.
-from django.http import HttpResponse, JsonResponse
+from django.http import HttpResponse, JsonResponse , HttpResponseRedirect
+from website.models import Contact
+from website.forms import NameForm, ContactForm, NewsletterForm
 
 def index_view(request):
     return render(request,'website/index.html')
@@ -10,7 +10,30 @@ def about_view(request):
     return render(request,'website/about.html')
 
 def contact_view(request):
-    return render(request,'website/contact.html')
+    if request.method=='POST':
+        form=ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+    form=ContactForm()
+    return render(request,'website/contact.html',{'form':form})
 
 def test_view(request):
-    return render(request,'website/test.html', {'name':'marjan', 'lastname':'fathi'})
+    if request.method=='POST':
+        form= ContactForm(request.POST)
+        if form.is_valid():
+            form.save() 
+            return HttpResponseRedirect('done')
+        else:
+            return HttpResponseRedirect('not valid')
+        
+    form= ContactForm()
+    return render(request,'test.html', {'form':form})
+
+def newsletter_view(request):
+    if request.method=='POST':
+        form= NewsletterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/')
+        else:
+            return HttpResponse('/')
